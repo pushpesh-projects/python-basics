d87172ea605895323a50100cb79c141e36eb9835
Pushpesh Kumar <Pushpesh.Kumar@dnb.no>
2022-12-26 00:30:46 +0100

    input function

---
+++ variables.py
@@ -0,0 +1,27 @@
+"""
+Each variable must have a unique name - an identifier.
+
+A legal identifier name must be a non-empty sequence of characters,
+must begin with the underscore(_), or a letter,
+and it cannot be a Python keyword.
+
+The first character may be followed by underscores, letters, and digits.
+Identifiers in Python are case-sensitive.
+
+Python is a dynamically-typed language,
+which means you don't need to declare variables in it.
+
+To assign values to variables,you can use a simple assignment operator
+in the form of the equal (=) sign, i.e.,var = 1.
+
+You can combine text and variables using the + operator,
+and use the print() function to output strings and variables,
+
+You can also use compound assignment operators (shortcut operators)
+to modify values assigned to variables,
+i = i + 2 * j ⇒ i += 2 * j
+var = var / 2 ⇒ var /= 2
+rem = rem % 10 ⇒ rem %= 10
+j = j - (i + var + rem) ⇒ j -= (i + var + rem)
+x = x ** 2 ⇒ x **= 2
+"""
